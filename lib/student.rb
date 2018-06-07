05395c1c914db67de16f8ccbff9bbf4ba22b71b3
Megan Dotto <megan.dotto@gmail.com>
2018-06-07 06:38:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,19 @@ class Student
     student = self.new(row[0], row[1], row[2])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
